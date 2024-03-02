5a30c1c77fd56c0f9f473443f998feb92ab85894
Forrest Herman <forrestherman13@gmail.com>
2024-03-02 17:52:29 -0500

    updated postrev0

--- data_col.py
+++ data_col.py
@@ -1,5 +1,4 @@
 import cv2
-import RPi.GPIO as GPIO
 import sys
 import os
 import time
@@ -9,15 +8,18 @@ start = 0
 start_flag = 1
 i = 0
 
+cwd = os.getcwd()
+
 while(1):
 
 	if(start_flag):
 		start_flag = 0
 		start = time.time()
 
-	if(time.time()-start % 10):
-
+	
+	if not(np.round(time.time()-start) % 20):
 		os.system('libcamera-still -t 100 -n -o train.jpg')
 		frame = cv2.imread("train.jpg")[0:1232, 0:1640]
-		cv2.imwrite("training" + str(i) + ".jpg", frame)
+		traindir = cwd+"/training" + str(i) + ".jpg"
+		cv2.imwrite(cwd+"/training/sample" + str(i) + ".jpg", frame)
 		i += 1    
--- main_loop.py
+++ main_loop.py
@@ -6,15 +6,15 @@ import time
 import numpy as np
 from parameters import slope, intercept
 
-MAX_TIME = 30
+MAX_TIME = 300
 T_SAMPLE = 10
+ERROR_BOUND = 5
 # Eventually these will just be hard coded
 slope = np.array(slope)
 intercept = np.array(intercept)
 
 left_done  = False
 right_done = False
-
 
 ## GPIO Config. ## 
 SOLENOID_IN = 26
@@ -31,7 +31,7 @@ GPIO.setup(RIGHT_CNTRL, GPIO.OUT)
 GPIO.output(LEFT_CNTRL, GPIO.LOW)
 GPIO.output(RIGHT_CNTRL, GPIO.LOW)
 GPIO.output(SOLENOID_OUT, GPIO.HIGH)
-####
+#### 
 
 def crispiness_to_colour(crispiness):
     return np.multiply(crispiness, slope) + intercept
@@ -42,17 +42,22 @@ def expand_colour_range(colour, tolerance = 20):
         ranges.append([colour[i] - tolerance, colour[i] + tolerance])
     return ranges
 
-def toast_done_left():
-    GPIO.output(LEFT_CNTRL, GPIO.LOW)
-    print("Left Done!")
-
-def toast_done_right():
-    GPIO.output(RIGHT_CNTRL, GPIO.LOW)
-    print("Right Done!")
-
 def eject():
     GPIO.output(SOLENOID_OUT, GPIO.LOW)
     print("Ejected!")
+    
+def compare(state, color_current, blue, green, red, gpio_pin):
+    if state == False and blue[0] < color_current[0] < blue[1] and green[0] < color_current[1] < green[1] and red[0] < color_current[2] < red[1]:
+        GPIO.output(gpio_pin, GPIO.LOW)
+        return True 
+    return state
+
+def compare2(state, current, final, bound, pin):
+    error = np.linalg.norm(current-final)
+    if state == False and error <= bound:
+        GPIO.output(pin, GPIO.LOW)
+        return True
+    return state
 
 crispiness = float(sys.argv[1])
 
@@ -61,12 +66,15 @@ if(not(0 <= crispiness <= 1)):
     GPIO.cleanup()
     exit()
 
-ranges = expand_colour_range(crispiness_to_colour(crispiness))
+target = crispiness_to_colour(crispiness)
+
+ranges = expand_colour_range(target)
+
 blue_range, green_range, red_range = ranges[0], ranges[1], ranges[2]
 print("Target:\t\t", crispiness_to_colour(crispiness))
 
 start_ctrl = 1
-start_time,dt = 0
+start_time, dt = 0, 0
 
 while(True): 
 
@@ -80,33 +88,31 @@ while(True):
         GPIO.output(RIGHT_CNTRL, GPIO.HIGH)
         time.sleep(0.5)
 
+        dt = np.round(time.time()-start_time)
 
-        if not(dt%T_SAMPLE):
+        if not(dt % T_SAMPLE):
             # Capture the video frame by frame 
-            os.system('libcamera-still -t 100 -n -o test.jpg')
+            os.system('libcamera-still -t 100 -n -o capture.jpg')
             # Get average colour of each frame
-            frame = cv2.imread("test.jpg")
-            average1 = np.array(cv2.mean(frame[0:1232, 0:1640])[0:3])
-            average2 = np.array(cv2.mean(frame[1232:2464, 0:1640])[0:3])
+            frame = cv2.imread("capture.jpg")
+            left_avg = np.array(cv2.mean(frame[0:1232, 0:1640])[0:3])
+            right_avg = np.array(cv2.mean(frame[1232:2464, 0:1640])[0:3])
 
             # Print both averages
-            print("Left/Right:\t", average1, "/", average2, end='\r') #small left big right
+            print("Left/Right:\t", left_avg, "/", right_avg, end='\r') #small left big right
 
             # Done if average colour is within a certain range
-            if left_done == False and blue_range[0] < average1[0] < blue_range[1] and green_range[0] < average1[1] < green_range[1] and red_range[0] < average1[2] < red_range[1]:
-                left_done = True
-                toast_done_left()
             
-            if right_done == False and blue_range[0] < average2[0] < blue_range[1] and green_range[0] < average2[1] < green_range[1] and red_range[0] < average2[2] < red_range[1]:
-                right_done = True
-                toast_done_right()
+            # left_done = compare(left_done, left_avg, blue_range, green_range, red_range, LEFT_CNTRL)
+            # right_done = compare(right_done, right_avg, blue_range, green_range, red_range, RIGHT_CNTRL)
 
-            if left_done and right_done:
+            left_done = compare2(left_done, left_avg, target, ERROR_BOUND, LEFT_CNTRL)
+            # right_done = compare2(right_done, right_avg, target, ERROR_BOUND, RIGHT_CNTRL)
+            right_done = True  
+                
+            if left_done and right_done or dt > MAX_TIME:
                 eject()
                 break
-
-        if(dt=(time.time()-start_time)>MAX_TIME): #2.5 min until break (not forsure if this will stop the circuit may have to set gpio)
-            break
 
         # Quit with 'q'
         if cv2.waitKey(1) & 0xFF == ord('q'):
