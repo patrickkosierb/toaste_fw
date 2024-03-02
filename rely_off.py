eccbff919c27777176da89a64f552452abcee88f
Forrest Herman <forrestherman13@gmail.com>
2024-03-02 18:05:28 -0500

    on/off

---
+++ rely_off.py
@@ -0,0 +1,24 @@
+import cv2
+import RPi.GPIO as GPIO
+import sys
+import os
+
+
+## GPIO Config. ## 
+SOLENOID_IN = 26
+SOLENOID_OUT  = 27
+LEFT_CNTRL  = 22
+RIGHT_CNTRL = 23
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SOLENOID_IN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SOLENOID_OUT, GPIO.OUT)
+GPIO.setup(LEFT_CNTRL, GPIO.OUT)
+GPIO.setup(RIGHT_CNTRL, GPIO.OUT)
+
+GPIO.output(LEFT_CNTRL, GPIO.LOW)
+GPIO.output(RIGHT_CNTRL, GPIO.LOW)
+GPIO.output(SOLENOID_OUT, GPIO.LOW)
+
+
+GPIO.cleanup()
