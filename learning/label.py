fe2c9a18fb1d2074efca9dd60a9e6eceeb95de07
Patrick Kosierb <kosierbp@mcmaster.ca>
2024-04-03 00:28:08 -0400

    add filter

--- learning/label.py
+++ learning/label.py
@@ -1,42 +1,47 @@
 import cv2
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
 
 raw = os.getcwd()+"/data_raw/" 
+raw_dir = os.listdir(raw)
 label = os.getcwd()+"/data_label/"
 
 if __name__ == '__main__':
 
-    number_of_images = len(os.listdir(raw))
-    average_colours = []
-    first_last = []
-    crispinesses = np.array(np.linspace(0, 1, 2))
-    c = []
-
-    # get first and last data points
-    maxCrispframe = os.listdir(raw)[-1]
-    minCrispframe = os.listdir(raw)[0]
-
-    # get rbg values
-    first_last.append(cv2.mean(cv2.imread(raw+minCrispframe))[0:3])
-    first_last.append(cv2.mean(cv2.imread(raw+maxCrispframe))[0:3])
-
-    # lls
+    # lls based on first and last pciture
+    first_last = np.array([cv2.mean(cv2.imread(raw+raw_dir[0]))[0:3],cv2.mean(cv2.imread(raw+raw_dir[-1]))[0:3]])
+    crispinesses = np.array([0,1])
     crispinesses = np.vstack([crispinesses, np.ones(2)]).T
     slope, intercept = np.linalg.lstsq(crispinesses, first_last, rcond=None)[0]
-
-    # label training data with crispiness from lls
     M = slope[0]**2 + slope[1]**2 + slope[2]**2 
-    for picture in os.listdir(raw):
+
+    # solve for crispiness given rgb and lls eqn found above 
+    frames = []
+    c_raw = []
+    for picture in raw_dir:
         frame = cv2.imread(raw+picture)
-        average_colours = (cv2.mean(frame)[0:3])
-        y = average_colours - intercept
+        frames.append(frame)
+        y = cv2.mean(frame)[0:3] - intercept
+        crisp = (slope[0]*y[0] + slope[1]*y[1] + slope[2]*y[2])/M
+        c_raw.append(int(100*np.clip(crisp, 0, 1)))
 
-        crisp = np.clip((slope[0]*y[0] + slope[1]*y[1] + slope[2]*y[2])/M, 0, 1)
-        c.append(int(crisp*100))
+    # apply filter for noise and label data 
+    window_size = 31 
+    poly_order = 2 
+    c_filtered = savgol_filter(np.array(c_raw), window_length=window_size, polyorder=poly_order)
+    
+    for i in range(len(raw_dir)):
+        cv2.imwrite(label+"/crisp-"+str(int(c_filtered[i]))+".jpg",frames[i])
 
-        cv2.imwrite(label+"/crisp-"+str(c[-1])+".jpg",frame)
-
+    plt.plot(c_raw,marker='o', linestyle='', markersize=8)
+    plt.plot(c_filtered,marker='o', linestyle='', markersize=8)
+    plt.legend(["LLS Labeled Raw", "LLS Labeled Savgol"])
+    plt.title("Labeling Crispiness")
+    plt.xlabel("Picture number")
+    plt.ylabel("Crispiness")
+    plt.savefig("crisp.png")
 
 
 
